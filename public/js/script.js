583825fcefddb082a01b57dedac83995eef57e18
Kaido Iwamoto <h6824.kaido@gmail.com>
2013-03-30 17:13:15 +0900

    Add confirm dialog before you leave new/edit form.

--- public/js/script.js
+++ public/js/script.js
@@ -12,8 +12,27 @@
     });
     
     $('.delete-form').on('submit', function () {
+      submitFlag = true;
       var res = confirm('Are you sure?');
       return !!res;
     });
+    
+    var submitFlag = false;
+    postForm.on("submit", function () {
+      submitFlag = true;
+    });
+    
+    var $title, $body, title, body;
+    if (postForm.length > 0) {
+      $title = postForm.find("[name=title]");
+      $body = postForm.find("[name=body]");
+      title = $title.val();
+      body = $body.val();
+      $(window).on("beforeunload", function () {
+        if (!submitFlag && ($title.val() !== title || $body.val() !== body)) {
+          return "This post is not saved yet."
+        }
+      });
+    }
   });
 })(jQuery);
